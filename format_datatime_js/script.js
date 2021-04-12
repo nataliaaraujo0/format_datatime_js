9d8c8ee3ebf2d92fc35c8e3dff6b26674e024635
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-04-11 21:30:32 -0300

    up_first_project
    Subindo arquivo pela primeira vez no git.

---
+++ format_datatime_js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', start);
+
+const clickArray = [];
+
+function start(){
+    const button = document.querySelector('#clickbutton');    
+    button.addEventListener('click', handleButtonClick);
+}
+
+function handleButtonClick(){
+    clickArray.push(getNemTimestamp());
+    render();
+}
+
+function render(){
+    const ul = document.querySelector('#data');
+    ul.innerHTML = '';
+
+    let lis = '';
+
+    clickArray.map((item) => {
+
+    lis +=  `<li>${item}</li>`; 
+    });
+
+    ul.innerHTML = lis;
+} 
+
+
+
+
+
---
+++ format_datatime_js/timestamp.js
@@ -0,0 +1,39 @@
+
+//2 - preencha algo do lado esquerdo do valor
+function leftPad(value, count = 2, char = '0'){
+    //4 - criar const para converter ints para string
+    const stringValue = value.toString();
+    let newValue = stringValue;
+    // 3 - condicao para colcoar zero  esqueda 
+    if (stringValue.length < count) {
+        for (let i = 0; i < count - stringValue.length; i++) {
+            newValue = char + stringValue;
+        }
+    }
+     return newValue;
+}
+
+
+//1 - function pegar data hora
+function getNemTimestamp() { 
+    const now = new Date();
+    let result = '';
+
+    //formatar data hora
+    result += leftPad(now.getDate());
+        result += '/';
+    result += leftPad(now.getMonth() +1);
+        result += '/';
+    result += now.getFullYear();
+        result += '';
+    result += leftPad(now.getHours());
+        result += ':';
+    result += leftPad(now.getMinutes());
+        result += ':';
+    result += leftPad(now.getSeconds());
+        result += '.';
+    result += leftPad(now.getMilliseconds(), 3);
+
+
+    return result;
+}
